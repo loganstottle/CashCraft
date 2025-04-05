ed7d849fbb3789b10882fdcc90ee07f31333ddb5
N-magic <n8martin2025@gmail.com>
2025-04-05 18:34:26 -0500

    Documented controller controller.go

--- controller/controller.go
+++ controller/controller.go
@@ -5,22 +5,31 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
+	"github.com/joho/godotenv" // I promise I will not leak an API key, I promise I will not leak an API key, I promise I will not leak an API key
 )
 
-func LoadEnv() {
+func LoadEnv() { // Imports those hidden secret enviroment variables
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Failed to load .env\n")
 	}
 }
 
+/*
+If you are self hosting you will need a .env file with
+FINNHUB_API_KEY
+DBUSER
+DBPW
+The database is MySql, and has been tested to work with MariaDB as well
+If you are on the prod dev team, reach out to the ginger via email or discord
+*/
+
 func StartServer() {
-	model.ConnectDatabase()
+	model.ConnectDatabase() // Model to connect to the database (We love MVC!)
 
-	app := fiber.New()
+	app := fiber.New() // Initialize the fiber app
 
-	SetupHomeRoutes(app)
-	SetupAuthRoutes(app)
-	app.Static("/*", "./view/404")
-	app.Listen(":3000")
+	SetupHomeRoutes(app)           // Nice organization to keep the routes in seperate files (less merge errors)
+	SetupAuthRoutes(app)           // Other halve of the routes
+	app.Static("/*", "./view/404") // For if someone puts in a wrong link (We are not case sensitive though - ease of use)
+	app.Listen(":3000")            // Starts the server to where people can connect to it
 }
