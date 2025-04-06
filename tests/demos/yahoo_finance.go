665748891b3c6bd1a58d9fa8f1385dbcb138c75c
N-magic <n8martin2025@gmail.com>
2025-04-05 20:16:15 -0500

    Added Easter Egg to Yahoo Finance

--- tests/demos/yahoo_finance.go
+++ tests/demos/yahoo_finance.go
@@ -1,14 +1,36 @@
+/*
+This is unused code from when we where hoping to use the yahoo api - but that went
+no where so it isn't included at all in docs about the project
+
+If you found this, congratulations! This is an easter egg
+
+Its dangerous to go alone!
+Take this...
+\****__              ____
+  |    *****\_      --/ *\-__
+  /_          (_    ./ ,/----'
+      \__         (_./  /
+         \__           \___----^__
+        _/   _                  \
+ |    _/  __/ )\"\ _____         *\
+ |\__/   /    ^ ^       \____      )
+  \___--"                    \_____ )
+
+Its the cute little dragon dude!
+*/
+
 package demos
 
 import (
-	"github.com/piquette/finance-go/quote"
 	"fmt"
+
+	"github.com/piquette/finance-go/quote"
 )
 
 func Yahoo() {
 	q, err := quote.Get("AAPL")
 	if err != nil {
-	  panic(err)
+		panic(err)
 	}
 
 	fmt.Println(q)
