3aa3e822fcdf956441f3a31fb14719609b451aff
N-magic <n8martin2025@gmail.com>
2025-04-05 20:04:01 -0500

    Model model.go is documented

--- model/model.go
+++ model/model.go
@@ -14,21 +14,21 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	user := os.Getenv("DBUSER")
+	user := os.Getenv("DBUSER") // Getting data out of the .env file - same next line
 	password := os.Getenv("DBPW")
-	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/cashcraft?parseTime=true", user, password)
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/cashcraft?parseTime=true", user, password) // Configures the connection to the database
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})                              // connection to the database
 
 	if err != nil {
-		panic("Failed to connect to database!")
+		panic("Failed to connect to database!") // Couldnt be more clearn than this, if error isnt no, go AGGGGHHHHHHH
 	}
 
-	database.AutoMigrate(&User{}, &Stock{}, &StockPrice{})
+	database.AutoMigrate(&User{}, &Stock{}, &StockPrice{}) // This is something that automatically turns structs into database - very cool
 
-	DB = database
+	DB = database // I question this line, but I do not touch it (Care to explain this anyone? - The Ginger)
 }
 
-func HashPassword(input string) string {
+func HashPassword(input string) string { // sha256 hashing for passwords - the industry standard
 	hasher := sha256.New()
 	hasher.Write([]byte(input))
 	hashedPassword := hex.EncodeToString(hasher.Sum(nil))
